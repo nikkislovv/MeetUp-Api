dcb55884a7eedf0d06715d369ef76282d3dcf787
Nikita <99506812+nikkislovv@users.noreply.github.com>
2022-08-12 14:19:04 +0300

    Adding Event Entity

---
+++ Entities/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Organizer { get; set; }
+        public DateTime Time { get; set; }
+        public string Place { get; set; }
+    }
+}
